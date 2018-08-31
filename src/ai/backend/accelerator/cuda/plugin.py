a7d8d09ba1c53d03c1db3635e40bbb4c87792765
Joongi Kim <joongi@lablup.com>
2018-08-31 21:51:15 +0900

    Add missing nvidia-docker version detection

--- src/ai/backend/accelerator/cuda/plugin.py
+++ src/ai/backend/accelerator/cuda/plugin.py
@@ -1,6 +1,22 @@
+import subprocess
+import logging
+
 from ai.backend.agent.accelerator import accelerator_types
 from .gpu import CUDAAccelerator
 
+log = logging.getLogger('ai.backend.accelerator.cuda')
+
 
 async def init(etcd):
+    try:
+        ret = subprocess.run(['nvidia-docker', 'version'],
+                             stdout=subprocess.PIPE)
+    except FileNotFoundError:
+        log.info('nvidia-docker is not installed.')
+        return 0
+    rx = CUDAAccelerator.rx_nvdocker_version
+    for line in ret.stdout.decode().strip().splitlines():
+        m = rx.search(line)
+        if m is not None:
+            CUDAAccelerator.nvdocker_version = tuple(map(int, m.group(1).split('.')))
     accelerator_types['cuda'] = CUDAAccelerator
