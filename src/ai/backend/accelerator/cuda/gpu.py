398ff1c9f9fe68b271e38c94c0277ee58b944890
Joongi Kim <joongi@lablup.com>
2018-09-04 12:13:25 +0900

    Simplify decimal rounding

--- src/ai/backend/accelerator/cuda/gpu.py
+++ src/ai/backend/accelerator/cuda/gpu.py
@@ -1,4 +1,4 @@
-import decimal
+from decimal import Decimal, ROUND_DOWN, ROUND_UP
 import logging
 from pathlib import Path
 import re
@@ -22,16 +22,14 @@ class CUDAAcceleratorInfo(AbstractAcceleratorInfo):
     unit_memory = (2 * (2 ** 30))  # 1 unit = 2 GiB
     unit_proc = 8                  # 1 unit = 8 SMPs
 
-    def max_share(self) -> decimal.Decimal:
+    def max_share(self) -> Decimal:
         mem_shares = self.memory_size / self.unit_memory
         proc_shares = self.processing_units / self.unit_proc
         common_shares = min(mem_shares, proc_shares)
-        with decimal.localcontext() as ctx:
-            ctx.rounding = decimal.ROUND_DOWN
-            quantum = decimal.Decimal('1.00')
-            return decimal.Decimal(common_shares).quantize(quantum)
+        quantum = Decimal('.01')
+        return Decimal(common_shares).quantize(quantum, ROUND_DOWN)
 
-    def share_to_spec(self, share: decimal.Decimal) -> (int, int):
+    def share_to_spec(self, share: Decimal) -> (int, int):
         # TODO: consider the memory margin for heap size?
         return (
             int(self.unit_memory * share),
@@ -39,14 +37,12 @@ class CUDAAcceleratorInfo(AbstractAcceleratorInfo):
         )
 
     def spec_to_share(self, requested_memory: int,
-                      requested_proc_units: int) -> decimal.Decimal:
+                      requested_proc_units: int) -> Decimal:
         mem_share = requested_memory / self.unit_memory
         proc_share = requested_proc_units / self.unit_proc
         required_share = max(mem_share, proc_share)
-        with decimal.localcontext() as ctx:
-            ctx.rounding = decimal.ROUND_UP
-            quantum = decimal.Decimal('1.00')
-            return decimal.Decimal(required_share).quantize(quantum)
+        quantum = Decimal('.01')
+        return Decimal(required_share).quantize(quantum, ROUND_UP)
 
 
 class CUDAAccelerator(AbstractAccelerator):
